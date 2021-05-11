bb7bbc287045b5419cf4914976cc355a9a3d4033
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-05-11 10:21:04 +0200

    chore: adding psql support, not done

---
+++ migrations/psql/1_init_dev.sql
@@ -0,0 +1,14 @@
+SELECT 'CREATE DATABASE session_db_dev'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'session_db_dev')\gexec ;
+
+\c session_db_dev;
+
+CREATE TABLE public.sessions_table(
+    id BIGSERIAL PRIMARY KEY,
+    session_id varchar(255) UNIQUE NOT NULL,
+    user_id varchar(255) NOT NULL,
+    ad_break_dur varchar(255) NOT NULL,
+    created varchar(255) NOT NULL,
+    cli_req varchar(255) NOT NULL,
+    response varchar(255) NOT NULL
+);
