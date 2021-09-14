118ff7281763482477984b3c64f368a6b424dcbf
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-09-14 08:54:42 +0200

    chore:

--- test/Session.spec.js
+++ test/Session.spec.js
@@ -4,7 +4,11 @@ const should = chai.should();
 const chaiMatchPattern = require("chai-match-pattern");
 const _ = chaiMatchPattern.getLodashModule();
 
+<<<<<<< HEAD
 mockClientParams1 = {
+=======
+clientParams1 = {
+>>>>>>> ba2d2d0 (test: added chai unittest)
   console: true,
   dur: "25",
   uid: "waitress-456",
@@ -13,7 +17,11 @@ mockClientParams1 = {
   ss: "1920x1080",
   uip: "123.23.32.13",
 };
+<<<<<<< HEAD
 mockClientParams2 = {
+=======
+clientParams2 = {
+>>>>>>> ba2d2d0 (test: added chai unittest)
   console: true,
   dur: "25",
   uid: "charlie-123",
@@ -23,6 +31,7 @@ mockClientParams2 = {
   uip: "193.123.32.153",
 };
 
+<<<<<<< HEAD
 mockTrackedEvent1 = {
   type: "start",
   issuedAt: "Today",
@@ -82,4 +91,13 @@ describe("SESSION CLASS", () => {
   });
 
 
+=======
+describe("SESSION CLASS", () => {
+  it("should create unique session ids", (done) => {
+    session1 = new Session(clientParams1);
+    session2 = new Session(clientParams2);
+    session1["sessionId"].should.not.equal(session2["sessionId"]);
+    done();
+  });
+>>>>>>> ba2d2d0 (test: added chai unittest)
 });
