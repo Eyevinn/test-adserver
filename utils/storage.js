f119f52f65930c947368759b3affeade638ed07b
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2021-09-15 10:51:30 +0200

    log clean up

--- utils/storage.js
+++ utils/storage.js
@@ -8,10 +8,10 @@ if (process.env.NODE_ENV !== "production") {
 
 let DBAdapter = null;
 if (process.env.APP_DB_PSQL_URL) {
-    console.log("Test-Adserver using PSQL storage...")
+    console.log("Test Adserver using PSQL storage...")
     DBAdapter = new PsqlDBAdapter();
 } else {
-    console.log("Test-Adserver using MEMORY storage...")
+    console.log("Test Adserver using MEMORY storage...")
     DBAdapter = new MemoryDBAdapter();
 }
 
